d18f5f9ae848fc4ca28f713f6309d4f36eafc9c4
minerstat <info@minerstat.com>
2018-08-17 19:40:27 +0200

    Build fix

--- minerstat/Program.cs
+++ minerstat/Program.cs
@@ -66,7 +66,7 @@ namespace minerstat {
   [STAThread]
   static void Main(string[] args) {
 
-            if (args.Length != 0)
+            if (args.Length == 0)
             {
                 MessageBox.Show("ERROR => Please, Start with minerstat.exe");
                 Application.Exit();
